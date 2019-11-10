43c30f6dd1aa10ba0638f6f02b35fa0bb6f3682f
Dinusha.v <thiwanka.dinusha@gmail.com>
2019-11-10 20:18:06 +0530

    getting user inputs from command lines

--- src/main/scala/com/creative/App.scala
+++ src/main/scala/com/creative/App.scala
@@ -3,33 +3,58 @@ package com.creative
 import bookstore.model.Book
 
 /**
- * @author ${user.name}
- */
+  * @author ${user.name}
+  */
 object App {
 
   def main(args : Array[String]) {
-    println( "Welcome to the Book Store\n" )
+    println( "*** Welcome to the Book Store ***\n" )
 
-    println( "Please enter book data according to the below mentioned properties \n" )
-    println( "1.Name \n" )
-    println( "2.Author \n" )
-    println( "3.Price \n" )
-    println( "4.Description \n" )
+    optionList
 
-    // creating book object
-    val name = args(0);
-    val author = args(1);
-    val price = args(2).toInt;
-    val description = args(3);
+    def optionList(): Unit ={
+      println( "Please enter 1 to enter book details" )
+      println( "Please enter 2 to exit" )
 
-    // creating book object and publishing
-    val  book: Book = new Book(name,author,price,description)
-    Sender.runSender(book);
+      val input: String = scala.io.StdIn.readLine("Please select the option(1 or 2) ")
+      println();
 
+      if(1 == input.toInt){
+        sendBookData
+      }else
+      {
+        println( "Exit" )
+      }
+    }
 
+    //    sendBookData(name,author,price,description)
+    //    def sendBookData(name:String, author:String, price:String, description:String  ): Unit ={
+    def sendBookData(): Unit ={
 
+      var name: String = scala.io.StdIn.readLine("Please enter book name  : ")
+      var author: String = scala.io.StdIn.readLine("Please enter author's name  : ")
+      var price: String = scala.io.StdIn.readLine("Please enter book price  : ")
+      var description: String = scala.io.StdIn.readLine("Please enter book description  : ")
 
+      while(!isAllDigits(price)){
+        price = scala.io.StdIn.readLine("Please enter number for price : ")
+      }
+      // creating book object and publishing
+      var  book: Book = new Book(name,author,price.toInt,description)
+      Sender.runSender(book);
 
+      optionList
+      println();
+      println();
+    }
+
+    /**
+      * This method is used to check whether the
+      * given string is a decimal number of not
+      * @param x
+      * @return boolean
+      */
+    def isAllDigits(x: String) = x forall Character.isDigit
   }
 
 }
