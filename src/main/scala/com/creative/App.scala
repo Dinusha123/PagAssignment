ded6b348d519f3220b2e9594086ba1b507866238
Dinusha.v <thiwanka.dinusha@gmail.com>
2019-11-10 18:32:35 +0530

    executable

--- src/main/scala/com/creative/App.scala
+++ src/main/scala/com/creative/App.scala
@@ -8,16 +8,28 @@ import bookstore.model.Book
 object App {
 
   def main(args : Array[String]) {
-    println( "Welcome to the Book Store" )
-    // You pass any thing at runtime
-    // that will be print on the console
-    for(arg<-args)
-    {
-      println(arg);
+    println( "Welcome to the Book Store\n" )
 
-      val  book: Book = new Book(200,arg,"Author Sample"+arg,2500,"Sample Description "+arg)
-      Sender.runSender(book);
-    }
+    println( "Please enter book data according to the below mentioned properties \n" )
+    println( "1.Name \n" )
+    println( "2.Author \n" )
+    println( "3.Price \n" )
+    println( "4.Description \n" )
+
+    // creating book object
+    val name = args(0);
+    val author = args(1);
+    val price = args(2).toInt;
+    val description = args(3);
+
+    // creating book object and publishing
+    val  book: Book = new Book(name,author,price,description)
+    Sender.runSender(book);
+
+
+
+
+
   }
 
 }
